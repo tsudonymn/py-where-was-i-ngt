44f46779b617b044a165592fb9d7494c520a97ad
Chris <tsudonymn@gmail.com>
2025-01-02 13:39:34 -0500

    A locations format

---
+++ locations.py
@@ -0,0 +1,12 @@
+target_locations = [
+    {
+        "name": "Location 1",
+        "coords": (42.25050654199762, -83.76497750923687),
+        "radius_km": 1.0
+    },
+    {
+        "name": "Location 2",
+        "coords": (42.292341316945816, -83.73343119024464),
+        "radius_km": 1.0
+    }
+]
